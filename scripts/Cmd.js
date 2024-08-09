47764ae319e649f4c046f1f25061002dacd1b4c6
Lazygojo28 <joshuajoves10@gmail.com>
2024-08-09 23:15:09 +0800

    Update Cmd.js

--- scripts/Cmd.js
+++ scripts/Cmd.js
@@ -1,1 +1,20 @@
-
+module.exports = {
+	config: {
+			name: "Cmd.js",
+			version: "1.0",
+			author: "Angelic Lao",
+			countDown: 5,
+			role: 0,
+			shortDescription: "sarcasm",
+			longDescription: "sarcasm",
+			category: "reply",
+	},
+onStart: async function(){}, 
+onChat: async function({
+	event,
+	message,
+	getLang
+}) {
+	if (event.body && event.body.toLowerCase() == "lf mm") return message.reply("gc");
+}
+};
